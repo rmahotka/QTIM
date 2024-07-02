5a7e3e141e6c0a2afeeebea17101e6a23d25dabf
rmahotka <rmahotka@gmail.com>
2024-07-02 19:56:45 +0300

    fix pages post

--- pages/posts/[id].vue
+++ pages/posts/[id].vue
@@ -2,11 +2,11 @@
   <div class="container">
     <div v-if="pending" class="panding"><span class="spiner"></span></div>
     <div v-else class="block-post">
-      <h1 class="title">{{ post.title }}</h1>
-      <img :src="post.image" :alt="post.title" />
+      <h1 class="title">{{ post?.title }}</h1>
+      <img :src="post?.image" :alt="post?.title" />
       <div class="post-block-text">
         <p class="post-block-title">About</p>
-        <p class="post-text">{{ post.description }}</p>
+        <p class="post-text">{{ post?.description }}</p>
       </div>
     </div>
   </div>
@@ -17,7 +17,7 @@ import type { IInfoPost } from "~/Types";
 const route = useRoute();
 
 const id = route.params.id;
-const { pending, data: post } = await useLazyFetch<IInfoPost[]>(
+const { pending, data: post } = await useLazyFetch<IInfoPost>(
   `https://6082e3545dbd2c001757abf5.mockapi.io/qtim-test-work/posts/${id}`
 );
 </script>
